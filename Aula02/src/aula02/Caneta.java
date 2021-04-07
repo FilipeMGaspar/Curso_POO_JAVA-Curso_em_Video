c79bcd864338d05abb29027c30058680fce84e05
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-07 22:39:01 +0100

    Aula 02b​ - Criando Classes e Objetos em Java

--- Aula02/src/aula02/Caneta.java
+++ Aula02/src/aula02/Caneta.java
@@ -18,10 +18,10 @@ public class Caneta {
     }
     
     void tampar(){
-        
+        this.tampada = true;
     }
     
     void destampar(){
-        
+        this.tampada = false;
     }
 }
