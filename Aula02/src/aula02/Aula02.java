41d8ae9c88080c0629c56810a74b4ea3b0b81aef
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-07 22:49:15 +0100

    Aula 02b​ - Criando Classes e Objetos em Java

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -8,7 +8,8 @@ public class Aula02 {
         c1.cor= "Azul";
         c1.ponta = 0.5f;//Referencia a atributo
         c1.tampar(); //Referencia a método
-        c1.status(); //Referencia a método
+        //c1.status(); //Referencia a método
+        c1.rabiscar();
     }
     
 }
