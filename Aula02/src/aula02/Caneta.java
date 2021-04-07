4a255b1d2ffbdd9de1e3d7954bd00ef708db25b0
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-07 22:28:58 +0100

    Aula 02b​ - Criando Classes e Objetos em Java

--- Aula02/src/aula02/Caneta.java
+++ Aula02/src/aula02/Caneta.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula02;
-
-/**
- *
- * @author Aprender
- */
 public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
     
+    void rabiscar(){
+        
+    }
+    
+    void tampar(){
+        
+    }
+    
+    void destampar(){
+        
+    }
 }
