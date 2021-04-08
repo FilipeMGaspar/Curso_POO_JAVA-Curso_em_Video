9b2e6e5a10ed730d3026db35230be609ee5ec0cd
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-08 21:07:58 +0100

    Aula 02b​ - Exercício - Objeto Concreto

--- Aula02/src/aula02exercicio/ClasseObjAbstrato.java
+++ Aula02/src/aula02exercicio/ClasseObjAbstrato.java
@@ -1,5 +1,12 @@
 package aula02exercicio;
 
 public class ClasseObjAbstrato {
+    // Classe objeto Abstrato
+        // Aluno Academia
+    
+    String nome;
+    int idade;
+    float peso;
+    
     
 }
--- Aula02/src/aula02exercicio/ObjAbstrato.java
+++ Aula02/src/aula02exercicio/ObjAbstrato.java
@@ -1,5 +1,7 @@
 package aula02exercicio;
 
 public class ObjAbstrato {
-    
+   public static void main(String[] args) {
+   
+   }
 }
