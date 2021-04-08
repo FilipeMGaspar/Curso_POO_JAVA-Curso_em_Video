7d6104695f5d123be712ea7e87d2bec41968ea7e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-08 21:12:35 +0100

    Aula 02b​ - Exercício - Objeto Concreto

--- Aula02/src/aula02exercicio/ClasseObjAbstrato.java
+++ Aula02/src/aula02exercicio/ClasseObjAbstrato.java
@@ -7,11 +7,16 @@ public class ClasseObjAbstrato {
     String nome;
     int idade;
     float peso;
+    boolean inscrito;
     
     void inscricao(){
         System.out.println("Nome: " + this.nome);
         System.out.println("Idade: " + this.idade);
         System.out.println("Peso: " + this.peso);
+    }
+    
+    void increver(){
+        this.inscrito = true;
     }
     
 }
