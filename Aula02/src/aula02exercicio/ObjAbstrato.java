def6d2aad816695e1bb61917569d00115a0ee75e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-08 21:16:24 +0100

    Aula 02b​ - Exercício - Objeto Abstrato

--- Aula02/src/aula02exercicio/ObjAbstrato.java
+++ Aula02/src/aula02exercicio/ObjAbstrato.java
@@ -2,6 +2,8 @@ package aula02exercicio;
 
 public class ObjAbstrato {
    public static void main(String[] args) {
-   
+       
+       ClasseObjAbstrato treinoLivre = new ClasseObjAbstrato();
+       
    }
 }
