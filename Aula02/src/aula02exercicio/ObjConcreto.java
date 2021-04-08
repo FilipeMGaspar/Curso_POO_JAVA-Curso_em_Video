3f759072929cf2b6e0ee61ae21586b24c38cd27f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-08 21:02:51 +0100

    Aula 02b​ - Exercício - Objeto Concreto

--- Aula02/src/aula02exercicio/ObjConcreto.java
+++ Aula02/src/aula02exercicio/ObjConcreto.java
@@ -1,7 +1,8 @@
 package aula02exercicio;
 
 public class ObjConcreto {
-       public static void main(String[] args) {
+    public static void main(String[] args) {
+        
         ClassObjConcreto fordFocus = new ClassObjConcreto();        
         fordFocus.criarMarca("Ford");
         fordFocus.criarModelo("Focus");
