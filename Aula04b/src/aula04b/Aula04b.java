d39f5c810b0d86a0737b5c91344b17fde1d663c0
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-16 20:40:12 +0100

    Aula 04b​ - Métodos Getter, Setter e Construtor

--- Aula04b/src/aula04b/Aula04b.java
+++ Aula04b/src/aula04b/Aula04b.java
@@ -5,6 +5,10 @@ public class Aula04b {
     public static void main(String[] args) {
        Caneta c1 = new Caneta("NIC","Amarela",0.4f);
        c1.status();
+       
+       Caneta c2 = new Caneta("KKK","Laranja",0.5f);
+       System.out.println("");
+       c2.status();
     }
     
 }
