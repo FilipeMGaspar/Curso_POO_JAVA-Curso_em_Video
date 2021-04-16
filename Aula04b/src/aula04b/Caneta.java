2f8a9895974ebd18323871b062d6fed9a5997644
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-16 19:41:06 +0100

    Aula 04b​ - Métodos Getter, Setter e Construtor

--- Aula04b/src/aula04b/Caneta.java
+++ Aula04b/src/aula04b/Caneta.java
@@ -1,6 +1,10 @@
 package aula04b;
 
 public class Caneta {
-   public String Modelo;
+   public String modelo;
    private float ponta;
+   
+   public String getModelo(){
+       return this.modelo;
+   }
 }
