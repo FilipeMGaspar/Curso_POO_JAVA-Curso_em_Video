04fe346884f7d6d4d805b8045554470d37bcb5df
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-16 20:21:33 +0100

    Aula 04b​ - Métodos Getter, Setter e Construtor

--- Aula04b/src/aula04b/Aula04b.java
+++ Aula04b/src/aula04b/Aula04b.java
@@ -4,14 +4,7 @@ public class Aula04b {
 
     public static void main(String[] args) {
        Caneta c1 = new Caneta();
-       c1.setModelo("Bic");
-        //c1.modelo="BIC"; //Atributo privado
-       
-        c1.setPonta(0.5f);
-        //c1.ponta = 0.5f; //Atributo privado
-        
-        //c1.status();        
-        System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta "+c1.getPonta());
+       c1.status();
     }
     
 }
