c73b6c8baced1f7bf46422682359d95b6385ff89
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-19 20:14:40 +0100

    Aula 05b​ - Exemplo Prático em Java

--- Aula05b/ContaBanco/src/contabanco/ContaBanco.java
+++ Aula05b/ContaBanco/src/contabanco/ContaBanco.java
@@ -1,5 +1,11 @@
 package contabanco;
 
 public class ContaBanco {
-    
+   public int numConta;
+   protected String tipoConta;
+   private String donoConta;
+   private double saldoConta;
+   private boolean status;
+   
+   
 }
