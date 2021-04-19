4daad3f54f635d8b9d675855c67cb76e01dd30e7
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-19 20:38:40 +0100

    Aula 05b​ - Exemplo Prático em Java

--- Aula05b/ContaBanco/src/contabanco/ContaBanco.java
+++ Aula05b/ContaBanco/src/contabanco/ContaBanco.java
@@ -5,12 +5,12 @@ public class ContaBanco {
    protected String tipoConta;
    private String donoConta;
    private double saldoConta;
-   private boolean status;
+   private boolean statusConta;
    
    //Métodos especiais
    public ContaBanco(){
        this.saldoConta = 0; //Saldo igual a zero
-       this.status = false; //Conta fechada
+       this.statusConta = false; //Conta fechada
    }
    
    //Método get e set para o atributo numConta
@@ -44,4 +44,39 @@ public class ContaBanco {
    public void setSaldoConta(double valor){
        this.saldoConta = valor;
    }
+   
+   //Método get e set para o atributo status
+   public boolean getStatus(){
+       return this.statusConta;
+   }
+   public void setStatus(boolean status){
+       this.statusConta = status;
+   }
+   
+   //Métodos
+   //Método para abrir uma conta
+   public void abrirConta(String tpConta){
+       this.setTipoConta(tpConta);
+       this.setStatus(true);
+       
+       if(tpConta == "CC"){
+           this.setSaldoConta(50);
+       }else if (tpConta == "CP"){
+           this.setSaldoConta(150);
+       }
+   }
+   
+   //Método para fechar a conta
+   public void fecharConta(){
+       if(this.getSaldoConta() > 0){
+           System.out.println("Impossivel Fechar a conta!");
+           System.out.println("A conta tem de saldo: R$ " + this.getSaldoConta());
+           System.out.println("O saldo deve ser igual a R$ 0.");
+       }else if(this.getSaldoConta() < 0){
+           System.out.println("Impossivel Fechar a conta!");
+           System.out.println("Esta conta tem em divida: R$ " + this.getSaldoConta()); 
+           System.out.println("O saldo deve ser igual a R$ 0.");
+       }
+   }
+   
 }
