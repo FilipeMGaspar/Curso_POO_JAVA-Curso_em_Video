b3dfbeac65d4ec23f2344b6f378ed73062e48087
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-01 22:32:54 +0100

    Aula 07b​ - Objetos Compostos em Java

--- Aula07b/src/aula07b/Lutador.java
+++ Aula07b/src/aula07b/Lutador.java
@@ -11,6 +11,18 @@ public class Lutador {
     private int vitorias;
     private int derrotas;
     private int empates;
+
+    //métodos especiais
+    //método construtor
+    public Lutador(String nome, String nacionalidade, float altura, float peso, int vitorias, int derrotas, int empates) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
     
     
 }
