2472fb530c86b5528de4c87ddff4205ff4f86793
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-01 22:22:44 +0100

    Aula 07b​ - Objetos Compostos em Java

--- Aula07b/src/aula07b/Lutador.java
+++ Aula07b/src/aula07b/Lutador.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula07b;
 
-/**
- *
- * @author Aprender
- */
 public class Lutador {
+    //Atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    
     
 }
