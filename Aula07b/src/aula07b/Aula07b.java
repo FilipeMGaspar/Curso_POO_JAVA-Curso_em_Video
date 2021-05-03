7519970365a2c791399ad42144d7b491b87ff156
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-03 18:29:26 +0100

    Aula 07b​ - Objetos Compostos em Java

--- Aula07b/src/aula07b/Aula07b.java
+++ Aula07b/src/aula07b/Aula07b.java
@@ -8,7 +8,8 @@ public class Aula07b {
       
         Lutador l[] = new Lutador[7]; //array de lutadores
         l[0] = new Lutador("Pretty boy", "França", 31 ,1.75f, 68.9f, 11, 2, 1);
-        //l[0].status();
+        l[0].ganharLuta();
+        l[0].status();
         
         l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
         //l[1].status();
