ddb646fd55220e84e71cc2851b424aaba24c55de
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-09 08:08:57 +0100

    Aula 08b​ - Agregação entre Objetos com Java

--- Aula07b/src/aula07b_e_08b/Luta.java
+++ Aula07b/src/aula07b_e_08b/Luta.java
@@ -14,4 +14,35 @@ public class Luta {
     public void lutar(){
         
     }
+    
+    //Métodos Getters e setters
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    
 }
