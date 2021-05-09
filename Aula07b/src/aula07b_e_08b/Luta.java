1ea22f6ce658895570253ad4ee783af251a0da68
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-09 08:05:32 +0100

    Aula 08b​ - Agregação entre Objetos com Java

--- Aula07b/src/aula07b_e_08b/Luta.java
+++ Aula07b/src/aula07b_e_08b/Luta.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula07b_e_08b;
-
-/**
- *
- * @author Aprender
- */
 public class Luta {
+    //Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
     
+    //Métodos publicos
 }
