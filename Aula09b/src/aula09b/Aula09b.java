205416fbef0d18919c5648e41ae3cd8bd1838005
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 12:32:01 +0100

    Aula 09b - Exercício prático POO em Java

--- Aula09b/src/aula09b/Aula09b.java
+++ Aula09b/src/aula09b/Aula09b.java
@@ -1,7 +1,7 @@
 package aula09b;
 
 public class Aula09b {
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
         Pessoa ana = new Pessoa("Ana", 25, 'F');
         ana.fichaPessoa();
         ana.fazerAniver();
@@ -10,8 +10,12 @@ public class Aula09b {
         Livro sql = new Livro("SLQ para iniciantes", "Gustavo Guanabara", 659);
         sql.detalhes();
         sql.abrir();
-        sql.avacarPag();
         
+        sql.setPagAtual(10);
+        sql.avacarPag(); //Avançar uma página       
+        
+        sql.setPagAtual(20);
+        sql.voltarPag(); //Recuar uma página
         //sql.folhear();
     }    
 }
