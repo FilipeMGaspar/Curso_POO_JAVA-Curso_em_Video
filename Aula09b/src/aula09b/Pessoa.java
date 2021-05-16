8d9e35528cbb572461ce93dc0fbecada84b83045
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 11:00:32 +0100

    Aula 09b - Exercício prático POO em Java

--- Aula09b/src/aula09b/Pessoa.java
+++ Aula09b/src/aula09b/Pessoa.java
@@ -5,5 +5,29 @@ public class Pessoa {
     private int idade;
     private char sexo;
     
+    //Métodos geters e seters
+    public String getNome() {
+        return nome;    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    
+    //Métodos Publicos
     
 }
