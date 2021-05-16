98e20da2574c67bc730ee7b13302452dda88a154
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 10:59:35 +0100

    Aula 09b - Exercício prático POO em Java

--- Aula09b/src/aula09b/Pessoa.java
+++ Aula09b/src/aula09b/Pessoa.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula09b;
 
-/**
- *
- * @author Aprender
- */
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private char sexo;
+    
     
 }
