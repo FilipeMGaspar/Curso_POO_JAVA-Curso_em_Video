d75c3d99a53f1aeb3bc6376c161b9de6e32f8660
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 10:57:10 +0100

    Aula 09b - Exercício prático POO em Java

---
+++ Aula09b/src/aula09b/Publicacao.java
@@ -0,0 +1,8 @@
+package aula09b;
+public interface Publicacao {
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void folhear();
+    public abstract void avacarPag();
+    public abstract void voltarPag();
+}
