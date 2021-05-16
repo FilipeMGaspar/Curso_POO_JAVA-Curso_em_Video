79165420564a6c70c3b72dab13388510d533b57f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-16 11:29:31 +0100

    Aula 09b - Exercício prático POO em Java

--- Aula09b/src/aula09b/Livro.java
+++ Aula09b/src/aula09b/Livro.java
@@ -36,7 +36,7 @@ public class Livro {
         return totPaginas;
     }
     public void setTotPaginas(int totPaginas) {
-        if(this.getTotPaginas() > 0){
+        if(totPaginas > 0){
            this.totPaginas = totPaginas; 
         }else{
             this.totPaginas = 0;   
@@ -57,4 +57,11 @@ public class Livro {
         this.aberto = aberto;
     }
     
+    //Métodos públicos
+    public void detalhes(){
+        System.out.println("\n **** Livro, detalhes ****");
+        System.out.println("Título: " + this.getTitulo());
+        System.out.println("Autor: " + this.getAutor());
+        System.out.println("Número de páginas: " + this.getTotPaginas());
+    }
 }
