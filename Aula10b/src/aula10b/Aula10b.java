fa4b5bd91b5994074a7efed6fde59d449e5b3a01
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-21 20:08:06 +0100

    Aula 10b - Herança (Parte 1) - Java

--- Aula10b/src/aula10b/Aula10b.java
+++ Aula10b/src/aula10b/Aula10b.java
@@ -5,13 +5,19 @@ public class Aula10b {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
         p1.setNome("João");
+        p1.setIdade(26);
+        p1.setSexo('M');
+        
         
         Professor p2 = new Professor();
         p2.setNome("Maria");
         p2.setEspecialidade("Artes");
         p2.setSalario(2500.52f);
-        p2.setIdade(36);
+        p2.setIdade(11);
         p2.setSexo('F');
+        p2.fichaProfessor();
+        p2.fazerAniv();
+        p2.fichaProfessor();
     }
     
 }
