5019db3fc055e1b44c43853a9eef7211b64765da
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-21 21:11:04 +0100

    Aula 10b - Herança (Parte 1) - Java

--- Aula10b/src/aula10b/Funcionario.java
+++ Aula10b/src/aula10b/Funcionario.java
@@ -4,4 +4,21 @@ public class Funcionario {
     //Atributos
     private String setor;
     private boolean trabalhando;
+    
+    //Métodos Geters e Seters
+
+    public String getSetor() {
+        return setor;    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+    
 }
