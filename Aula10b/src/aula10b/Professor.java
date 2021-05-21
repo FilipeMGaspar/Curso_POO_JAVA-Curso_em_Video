35c1d93cf187d38b0f67fad747da268edca5aaeb
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-21 20:06:30 +0100

    Aula 10b - Herança (Parte 1) - Java

--- Aula10b/src/aula10b/Professor.java
+++ Aula10b/src/aula10b/Professor.java
@@ -25,4 +25,14 @@ public class Professor extends Pessoa {
     public void receberAumento(float aumt){
         this.setSalario(this.getSalario() + aumt);
     }
+    
+    public void fichaProfessor(){
+        System.out.println("\n..: Ficha do Professor :..");
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Idade: " + this.getIdade());
+        System.out.println("Sexo: " + this.getSexo());
+        System.out.println("Especialidade: " + this.getEspecialidade());
+        System.out.println("Salário: R$ " + this.getSalario());
+        System.out.println("");
+    }
 }
