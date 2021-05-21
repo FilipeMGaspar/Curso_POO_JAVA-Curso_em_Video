c091d3ab96629dc9e5ce80afd59561f74b7dc938
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-21 19:59:06 +0100

    Aula 10b - Herança (Parte 1) - Java

--- Aula10b/src/aula10b/Professor.java
+++ Aula10b/src/aula10b/Professor.java
@@ -5,5 +5,20 @@ public class Professor {
     private String especialidade;
     private float salario;
     
+    //Métodos Geters e Seters
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
     
 }
