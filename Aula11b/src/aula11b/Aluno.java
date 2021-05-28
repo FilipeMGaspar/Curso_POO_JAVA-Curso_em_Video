ffd73ac961a6ef021bd507d3eb2d137702bbff13
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-28 14:24:46 +0100

    Aula 11b - Herança (Parte 2) - JAVA

--- Aula11b/src/aula11b/Aluno.java
+++ Aula11b/src/aula11b/Aluno.java
@@ -5,5 +5,21 @@ public class Aluno extends Pessoa {
     private int matricula;
     private String curso;
     
+    //Métodos geter e seter
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
     
 }
