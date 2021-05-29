7ba2eccd08b57dc4ad8085d938e490600d4dfad7
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-29 08:11:23 +0100

    Aula 11b - Herança (Parte 2) - JAVA

--- Aula11b/src/aula11b/Aula11b.java
+++ Aula11b/src/aula11b/Aula11b.java
@@ -6,11 +6,19 @@ public class Aula11b {
     public static void main(String[] args) {
         
         //Pessoa P1 = new Pessoa();
-        Visitante v1 = new Visitante();
+        /*Visitante v1 = new Visitante();
         v1.setNome("João");
         v1.setIdade(33);
         v1.setSexo('M');
-        System.out.println(v1.toString());
+        System.out.println(v1.toString());*/
+        
+        Aluno a1 = new Aluno();
+        a1.setNome("Cláudio");
+        a1.setMatricula(232115);
+        a1.setCurso("Informática");
+        a1.setIdade(16);
+        a1.setSexo('M');
+        a1.pagarMensalidade();
     }
     
 }
