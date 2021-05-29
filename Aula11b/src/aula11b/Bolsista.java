6811680163fb756e6e47688ceb70e14aa2bbd635
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-29 08:16:49 +0100

    Aula 11b - Herança (Parte 2) - JAVA

--- Aula11b/src/aula11b/Bolsista.java
+++ Aula11b/src/aula11b/Bolsista.java
@@ -4,5 +4,13 @@ public class Bolsista extends Pessoa {
     //Atributos
     private float bolsa;
     
+    //Métodos geter e seter
+    public float getBolsa() {
+        return bolsa;
+    }
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+    
     
 }
