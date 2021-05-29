07205492e4581973a982312bc1f06231024606fb
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-29 08:23:58 +0100

    Aula 11b - Herança (Parte 2) - JAVA

--- Aula11b/src/aula11b/Aula11b.java
+++ Aula11b/src/aula11b/Aula11b.java
@@ -19,6 +19,14 @@ public class Aula11b {
         a1.setIdade(16);
         a1.setSexo('M');
         a1.pagarMensalidade();
+        
+        Bolsista b1 = new Bolsista();
+        b1.setMatricula(512321);
+        b1.setNome("Jubileu");
+        b1.setBolsa(12.5f);
+        b1.setSexo('M');
+        b1.setCurso("TIM");
+        b1.pagarMensalidade();
     }
     
 }
