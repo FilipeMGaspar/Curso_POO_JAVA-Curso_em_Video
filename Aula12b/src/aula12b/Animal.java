9c3cb91ddddac5dcacf7f091e3ede190a84e6c7c
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-06 08:06:20 +0100

    Aula 12b - Polimorfismo em Java (Parte 1) - JAVA

--- Aula12b/src/aula12b/Animal.java
+++ Aula12b/src/aula12b/Animal.java
@@ -12,4 +12,26 @@ public abstract class Animal {
     public abstract void alimentar();
     public abstract void emitirSom();
     
+    //Métodos Geter e Seter
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+    
 }
