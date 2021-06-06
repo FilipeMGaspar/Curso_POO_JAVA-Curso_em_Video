fe37eed4e914692e116e05d22b7ff29550d7a1eb
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-06 08:09:37 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Mamifero.java
+++ Aula12b/src/aula12b/Mamifero.java
@@ -1,8 +1,23 @@
 package aula12b;
 
-public class Mamifero {
+public class Mamifero extends Animal {
     //Atributos
     private String corPelo;
     
+    //Métodos abstractos sobrescritos
+    @Override
+    public void locomover() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alimentar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void emitirSom() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
