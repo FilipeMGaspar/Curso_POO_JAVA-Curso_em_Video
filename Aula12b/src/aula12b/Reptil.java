49215c95ab7cd8957ecf161d349457a165959c64
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-06 08:20:17 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Reptil.java
+++ Aula12b/src/aula12b/Reptil.java
@@ -3,4 +3,13 @@ package aula12b;
 public class Reptil {
     //Atributos
     private String corEscama;
+    
+    //Métodos Geter e seter
+    public String getCorEscama() {
+        return corEscama;
+    }
+    public void setCorEscama(String corEscama) {
+        this.corEscama = corEscama;
+    }
+    
 }
