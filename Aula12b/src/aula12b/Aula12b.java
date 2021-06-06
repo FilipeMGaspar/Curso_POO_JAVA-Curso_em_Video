29c2c4b64359f105c132fb4eb0219e3b22257733
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-06 08:07:29 +0100

    Aula 12b - Polimorfismo em Java (Parte 1) - JAVA

--- Aula12b/src/aula12b/Aula12b.java
+++ Aula12b/src/aula12b/Aula12b.java
@@ -4,6 +4,8 @@ package aula12b;
 public class Aula12b {
 
     public static void main(String[] args) {
+        //Animal a = new Animal(); //Classes abstractas não podem ser instanciadas
+        
         
     }
     
