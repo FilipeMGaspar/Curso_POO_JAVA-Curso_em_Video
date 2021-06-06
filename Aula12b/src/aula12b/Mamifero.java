c3e37e87d1802be72f8efee922d902bee2daa96b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-06 08:17:42 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Mamifero.java
+++ Aula12b/src/aula12b/Mamifero.java
@@ -31,9 +31,9 @@ public class Mamifero extends Animal {
     //Método público
     public void detalhesMamifero(){
         System.out.println("Mamífero: ");
-        System.out.println("Peso: " + this.peso);
-        System.out.println("Idade: " + this.getIdade());
-        System.out.println("Membros: " + this.membros);
-        System.out.println("Cor do pelo: " + this.getCorPelo());
+        System.out.println("  Peso: " + this.peso);
+        System.out.println("  Idade: " + this.getIdade());
+        System.out.println("  Membros: " + this.membros);
+        System.out.println("  Cor do pelo: " + this.getCorPelo());
     }
 }
