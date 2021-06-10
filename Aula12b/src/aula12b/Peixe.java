9c27a85a5dec2b76fbbe5065d3f5b712b6bf13c0
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-10 10:23:38 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Peixe.java
+++ Aula12b/src/aula12b/Peixe.java
@@ -20,14 +20,17 @@ public class Peixe extends Animal {
     //Métodos Abstractos Sobrescritos
     @Override
     public void locomover() {
+        System.out.println("Nadando!");
     }
 
     @Override
     public void alimentar() {
+        System.out.println("Comendo substâncias!");
     }
 
     @Override
     public void emitirSom() {
+        System.out.println("Peixe não faz som!");
     }
     
     
