e39d21ae34c1ffef8fd13132fb7f5014ce3dcaf5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-10 10:32:04 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Ava.java
+++ Aula12b/src/aula12b/Ava.java
@@ -1,5 +1,15 @@
 package aula12b;
 
 public class Ava {
+    //Atributos
+    private String corDasPenas;
+    
+    //Métodos geter e seter
+    public String getCorDasPenas() {
+        return corDasPenas;
+    }
+    public void setCorDasPenas(String corDasPenas) {
+        this.corDasPenas = corDasPenas;
+    }
     
 }
