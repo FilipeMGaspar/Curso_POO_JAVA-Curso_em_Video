b03f7573bfda3f723c82cf4c53e4f7a766edde72
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-10 10:22:00 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Peixe.java
+++ Aula12b/src/aula12b/Peixe.java
@@ -1,6 +1,6 @@
 package aula12b;
 
-public class Peixe {
+public class Peixe extends Animal {
     //Atributos
     private String corDaEscama;
     
@@ -16,4 +16,22 @@ public class Peixe {
     public void soltarBolha(){
         System.out.println("Soltou uma bolha!");
     }
+
+    //Métodos Abstractos Sobrescritos
+    @Override
+    public void locomover() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alimentar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void emitirSom() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 }
