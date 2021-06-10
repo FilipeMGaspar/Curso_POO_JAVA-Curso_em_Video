6fff3c4870517f41aaac4b72ac3ef3dbcf6b35e8
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-10 10:20:11 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Peixe.java
+++ Aula12b/src/aula12b/Peixe.java
@@ -1,5 +1,15 @@
 package aula12b;
 
 public class Peixe {
+    //Atributos
+    private String corDaEscama;
+    
+    //Métodos Geter e Seter
+    public String getCorDaEscama() {
+        return corDaEscama;
+    }
+    public void setCorDaEscama(String corDaEscama) {
+        this.corDaEscama = corDaEscama;
+    }
     
 }
