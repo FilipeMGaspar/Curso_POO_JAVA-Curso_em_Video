a3f18ba821d0ae59e994f7d63a7b2a119d29f306
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-10 10:37:16 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Ave.java
+++ Aula12b/src/aula12b/Ave.java
@@ -20,17 +20,14 @@ public class Ave extends Animal {
     //Métodos abstractos sobrescritos
     @Override
     public void locomover() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void alimentar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void emitirSom() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
