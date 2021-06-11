bcba3aaee1befd8ae64361dd6541e043171d69c2
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-11 07:33:36 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Canguru.java
+++ Aula12b/src/aula12b/Canguru.java
@@ -1,7 +1,13 @@
 package aula12b;
 
-public class Canguru {
+public class Canguru extends Mamifero {
     public void usarBolsa(){
-        System.out.println("Usando bolsa");
+        System.out.println("Usando bolsa!");
+    }
+    
+    //Métodos abstractos sobrescritos
+    @Override
+    public void locomover() {
+        System.out.println("Correndo!");
     }
 }
