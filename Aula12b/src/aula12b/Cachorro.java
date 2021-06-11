68f6a8d56d923ebe612aa5e957f885e35813b149
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-11 07:46:52 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Cachorro.java
+++ Aula12b/src/aula12b/Cachorro.java
@@ -10,6 +10,7 @@ public class Cachorro extends Mamifero {
         System.out.println("Abanando o Rabo!");
     }
     
+    //Métodos abstractos sobrescritos
      @Override
     public void emitirSom() {
         System.out.println("Latir! Ão ão ão!");
