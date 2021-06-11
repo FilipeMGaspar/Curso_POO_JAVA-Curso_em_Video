12311415daf5241caf03c0251653ace9db40d9fc
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-11 07:51:42 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Aula12b.java
+++ Aula12b/src/aula12b/Aula12b.java
@@ -73,7 +73,9 @@ public class Aula12b {
         cao.alimentar();
         cao.locomover();
         cao.abanarRabo();
-        cao.enterrarOsso();   
+        cao.enterrarOsso();
+        
+        
     }
     
 }
