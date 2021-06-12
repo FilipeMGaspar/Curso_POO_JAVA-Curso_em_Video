8c21cb889672c33630678510d4a6acbc5a3f2a99
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-12 08:14:32 +0100

    Aula 12b - Polimorfismo em Java (Parte 1)

--- Aula12b/src/aula12b/Tartaruga.java
+++ Aula12b/src/aula12b/Tartaruga.java
@@ -1,5 +1,9 @@
 package aula12b;
 
-public class Tartaruga {
-    
+public class Tartaruga extends Reptil {
+    //Métodos Abstractos Sobrescritos
+    @Override
+    public void locomover() {
+        System.out.println("Rastejando!");
+    }
 }
