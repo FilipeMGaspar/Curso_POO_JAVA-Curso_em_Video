47d239ee6b0a1f753b13a2d3f207cd80a0d47876
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-20 08:07:53 +0100

    13b - Polimorfismo Sobrecarga Java (Parte 2)

--- Aula13b/src/aula13b/Aula13b.java
+++ Aula13b/src/aula13b/Aula13b.java
@@ -4,6 +4,15 @@ public class Aula13b {
 
     public static void main(String[] args) {
         //programa principal
+        
+        //Mamifero é uma especialização de Animal
+        Mamifero m = new Mamifero();
+        m.setIdade(4);
+        m.setPeso(36.52);
+        m.setMembros(4);
+        m.setCorDoPelo("Bege");
+        m.emitirSom();
+        m.detalhesMamifero();
     }
     
 }
