d868df9f2a4ff647a1e350e2d668b1820a2190af
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-20 07:59:19 +0100

    13b - Polimorfismo Sobrecarga Java (Parte 2)

--- Aula13b/src/aula13b/Mamifero.java
+++ Aula13b/src/aula13b/Mamifero.java
@@ -15,7 +15,6 @@ public class Mamifero extends Animal {
     //Métodos abestratos sobrescritos
     @Override
     public void emitirSom() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
