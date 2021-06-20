31597b9514f55cb10bbaa97ef9a5c8762fdcb312
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-20 07:58:10 +0100

    13b - Polimorfismo Sobrecarga Java (Parte 2)

--- Aula13b/src/aula13b/Mamifero.java
+++ Aula13b/src/aula13b/Mamifero.java
@@ -1,5 +1,15 @@
 package aula13b;
 
 public class Mamifero {
+    //Atributos
+    protected String corDoPelo;
+    
+    //Métodos geter e seter
+    public String getCorDoPelo() {
+        return corDoPelo;
+    }
+    public void setCorDoPelo(String corDoPelo) {
+        this.corDoPelo = corDoPelo;
+    }
     
 }
