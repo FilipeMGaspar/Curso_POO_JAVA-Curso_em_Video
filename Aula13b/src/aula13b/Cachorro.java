199453349055b7a9b4ace35deeced89b41161035
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-06-24 10:13:12 +0100

    Aula 13b - Polimorfismo Sobrecarga Java (Parte 2)

--- Aula13b/src/aula13b/Cachorro.java
+++ Aula13b/src/aula13b/Cachorro.java
@@ -1,5 +1,9 @@
 package aula13b;
 
 public class Cachorro extends Lobo {
-    
+        //Métodos Abstracto sobrescrito
+    @Override
+    public void emitirSom() {
+        System.out.println("Au! Au! Au!");
+    }
 }
