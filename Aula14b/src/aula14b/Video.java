f2774ed2fe9a3eb7526693a0d06da46e31b55e65
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:06:15 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Video.java
+++ Aula14b/src/aula14b/Video.java
@@ -7,6 +7,43 @@ public class Video implements AcoesVideo{
     private int views;
     private int curtidas;
     private boolean reproduzindo;
+    
+    //Métodos Geter e Seter
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+    
 
     //Métodos abstractos sobrescritos
     @Override
