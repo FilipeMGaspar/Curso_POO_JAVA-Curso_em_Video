74a52432202d85068f65d7059e69a6554b5522c2
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:05:00 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Video.java
+++ Aula14b/src/aula14b/Video.java
@@ -1,12 +1,28 @@
 package aula14b;
 
-public class Video {
+public class Video implements AcoesVideo{
     //Atributos
     private String titulo;
     private int avaliacao;
     private int views;
     private int curtidas;
     private boolean reproduzindo;
+
+    //Métodos abstractos sobrescritos
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void like() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
 }
