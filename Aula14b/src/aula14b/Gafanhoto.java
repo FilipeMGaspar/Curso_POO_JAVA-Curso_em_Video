303b3ecb354f6eaf7779d8b62cb42d35c97d8fd7
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:28:44 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Gafanhoto.java
+++ Aula14b/src/aula14b/Gafanhoto.java
@@ -1,9 +1,14 @@
 package aula14b;
 
-public class Gafanhoto {
+public class Gafanhoto extends Pessoa{
     //Atributos
     private String login;
     private int totAssistido;
+
+    //Método Construtor
+    public Gafanhoto(String nome, int idade, char sexo) {
+        super(nome, idade, sexo);
+    }    
     
     //Métodos Geter e Seter
     public String getLogin() {
