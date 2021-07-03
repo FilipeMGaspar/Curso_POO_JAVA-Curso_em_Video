01b0eb175db1f59bbd99f0a8a7fb4de9786cee52
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:10:56 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Aula14b.java
+++ Aula14b/src/aula14b/Aula14b.java
@@ -3,7 +3,10 @@ package aula14b;
 public class Aula14b {
 
     public static void main(String[] args) {
-        
+        Video v[] = new Video[3];
+        v[0] = new Video("Aula 1 de POO");
+        v[1] = new Video("Aula 12 de PHP");
+        V[2] = new Video("Aula 10 de HTML5");
     }
     
 }
