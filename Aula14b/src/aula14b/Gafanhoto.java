29b4b2889ddf0450f8f16af259d0f8d820d3dab9
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:27:19 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Gafanhoto.java
+++ Aula14b/src/aula14b/Gafanhoto.java
@@ -4,4 +4,20 @@ public class Gafanhoto {
     //Atributos
     private String login;
     private int totAssistido;
+    
+    //Métodos Geter e Seter
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+    
 }
