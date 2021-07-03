17687af060371564eeb3f43e4ea329802d6b4e68
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:03:50 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Video.java
+++ Aula14b/src/aula14b/Video.java
@@ -1,5 +1,12 @@
 package aula14b;
 
 public class Video {
+    //Atributos
+    private String titulo;
+    private int avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+    
     
 }
