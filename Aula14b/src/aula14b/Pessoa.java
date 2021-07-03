c305f98244a36ba1e70862233a6efb7a0aa756b7
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:22:29 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Pessoa.java
+++ Aula14b/src/aula14b/Pessoa.java
@@ -7,5 +7,34 @@ public class Pessoa {
     protected char sexo;
     protected float experiencia;
     
+    //Métodos Geter e Seter
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getExperiencia() {
+        return experiencia;
+    }
+    public void setExperiencia(float experiencia) {
+        this.experiencia = experiencia;
+    }
     
 }
