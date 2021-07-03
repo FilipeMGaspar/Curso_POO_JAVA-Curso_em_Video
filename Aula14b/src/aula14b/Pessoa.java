b3746a89c64051b7a65f40a328178acaa93b6612
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-03 18:20:02 +0100

    Aula 14b - Projeto Final em Java (Parte 1)

--- Aula14b/src/aula14b/Pessoa.java
+++ Aula14b/src/aula14b/Pessoa.java
@@ -1,5 +1,11 @@
 package aula14b;
 
 public class Pessoa {
+    //Atributos
+    protected String nome;
+    protected int idade;
+    protected char sexo;
+    protected float experiencia;
+    
     
 }
