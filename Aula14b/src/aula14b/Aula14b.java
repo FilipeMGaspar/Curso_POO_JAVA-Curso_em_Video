4b462eff079e09cec9120d898734b0ef31a2e981
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-10 09:16:59 +0100

    Aula 15b - Projeto Final em Java (Parte 2)

--- Aula14b/src/aula14b/Aula14b.java
+++ Aula14b/src/aula14b/Aula14b.java
@@ -7,12 +7,12 @@ public class Aula14b {
         v[0] = new Video("Aula 1 de POO");
         v[1] = new Video("Aula 12 de PHP");
         v[2] = new Video("Aula 10 de HTML5");
-        System.out.println(v[0].toString());
+        //System.out.println(v[0].toString());
         
         Gafanhoto g[] = new Gafanhoto[2];
         g[0] = new Gafanhoto("Jubileu", 18, 'M', "Jubas");
         g[1] = new Gafanhoto("Juliana", 25, 'F', "sexyJu");
-        System.out.println(g[0].toString());
+        //System.out.println(g[0].toString());
         
         Visualizacao vis = new Visualizacao(g[0], v[2]);
         System.out.println(vis.toString());
