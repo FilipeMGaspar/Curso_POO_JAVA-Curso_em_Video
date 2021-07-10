74638ccf3505a38682ca94e9b2ce4fd99ef4863c
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-10 09:23:57 +0100

    Aula 15b - Projeto Final em Java (Parte 2)

--- Aula14b/src/aula14b/Aula14b.java
+++ Aula14b/src/aula14b/Aula14b.java
@@ -17,7 +17,8 @@ public class Aula14b {
         Visualizacao vis[] = new Visualizacao[5];
         vis[0] = new Visualizacao(g[0], v[2]);
         System.out.println(vis[0].toString());
-        vis[0] = new Visualizacao(g[0], v[1]);
+        vis[1] = new Visualizacao(g[0], v[1]);
+        System.out.println(vis[1].toString());
     }
     
 }
