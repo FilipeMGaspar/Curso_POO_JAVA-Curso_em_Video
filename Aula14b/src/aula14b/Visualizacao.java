e53e67a6ea260c0f1d7502229887493a4a607672
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-07-10 09:12:05 +0100

    Aula 15b - Projeto Final em Java (Parte 2)

--- Aula14b/src/aula14b/Visualizacao.java
+++ Aula14b/src/aula14b/Visualizacao.java
@@ -4,4 +4,11 @@ public class Visualizacao {
     //Atributos
     private Gafanhoto espectador;
     private Video filme;
+    
+    //Método Construtor
+    public Visualizacao(Gafanhoto espectador, Video filme) {
+        this.espectador = espectador;
+        this.filme = filme;
+    }
+    
 }
